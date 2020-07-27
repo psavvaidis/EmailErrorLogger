bac5533a271a6478021265bd1eaea34d185dce66
psavvaidis <panagiotissav@gmail.com>
2020-07-27 15:24:04 +0300

    reformat shell menu

--- shell.py
+++ shell.py
@@ -9,18 +9,32 @@ def loadCampaign():
             return helpers.loadCampaign(campaign['id'])
 
 
+def printMenu():
+    return input(
+        'Menu:\n'
+        '1. List all Campaigns\n'
+        '2. Load a Campaign by title\n'
+        '3. Update a Campaign by title\n'
+        '4. Delete a Campaign by title\n'
+        '5. Send a custom message through a campaign\n'
+        '6. Exit\n\n'
+    )
+
+
+# The User Interface
 print("\n\tWelcome to Email Error Logger\n\n")
 service = LogService()
-option = input(
-    'Menu:\n'
-    '1. List all Campaigns\n'
-    '2. Load a Campaign by title\n'
-    '3. Update a Campaign by title\n'
-    '4. Delete a Campaign by title\n'
-    '5. Send a custom message through a campaign\n'
-    '6. Exit\n\n'
-)
-if str(option) == '1':
-    print(service.campaigns)
-elif str(option) == '2':
-    loadCampaign()
+while True:
+    option = printMenu()
+    if str(option) == '1':
+        print(service.campaigns)
+    elif str(option) == '2':
+        loadCampaign()
+    elif str(option) == '3':
+        pass
+    elif str(option) == '4':
+        pass
+    elif str(option) == '5':
+        pass
+    elif str(option) == '6':
+        exit()
